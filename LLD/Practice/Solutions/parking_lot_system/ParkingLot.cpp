bc20efa917b5b0e00a88d037a1dc9080fa965060
Ayush Chouhan <100469226+Ac-ayush@users.noreply.github.com>
2025-07-28 16:58:02 +0000

    Added Parking Lot class

---
+++ LLD/Practice/Solutions/parking_lot_system/ParkingLot.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class ParkingLot {
+  vector<ParkingSpot*> spots;
+  unordered_map<string, ParkingSpot*> occupiedSpotsdata;  // licence -> spot
+  int capacity;
+  int availableSpots;
+
+ public:
+  ParkingLot(int compactCount, int regularCount, int largeCount) {
+    this->capacity = compactCount + regularCount + largeCount;
+    this->availableSpots = 0;
+
+    for (int i = 0; i < compactCount; i++) {
+      spots.push_back(new ParkingSpot(i, SpotSize::COMPACT));
+    }
+    for (int i = 0; i < regularCount; i++) {
+      spots.push_back(new ParkingSpot(i, SpotSize::REGULAR));
+    }
+    for (int i = 0; i < largeCount; i++) {
+      spots.push_back(new ParkingSpot(i, SpotSize::LARGE));
+    }
+  }
+
+  int getcapacity() { return capacity; }
+
+  int getAvailableSpots() { return availableSpots; }
+
+  ParkingSpot* findAvailableSpot(Vehicle* vehicle) {
+    for (auto& spot : spots) {
+      if (spot->isAvailable && spot->canFitVehicle(vehicle)) {
+        return spot;
+      }
+    }
+    return nullptr;
+  }
+
+  bool parkVehicle(Vehicle* vehicle) {
+    if (!vehicle) return false;
+
+    // check if already parked
+    if (occupiedSpotsdata.find(vehicle->getLicenceNumber()) !=
+        occupiedSpotsdata.end()) {
+      return false;
+    }
+
+    if (availableSpots < 1) {
+      cout << "No spots available" << endl;
+      return false;
+    }
+
+    // check for avaialble spot
+    ParkingSpot* spotToPark = findAvailableSpot(vehicle);
+
+    if (!spotToPark) return false;
+
+    if (spotToPark->parkVehicle(vehicle)) {
+      occupiedSpotsdata[vehicle->getLicenceNumber()] = vehicle;
+      availableSpots--;
+      return true;
+    }
+  }
+
+  Vehicle* removeVehicle(string licenceNumber) {
+    auto it = occupiedSpotsdata.find(licenceNumber);
+    if (it == occupiedSpotsdata.end()) return nullptr;
+
+    ParkingSpot* spot = it->second;
+    Vehicle* vehicle = spot->removeFromSpot();
+
+    if (vehicle) {
+      occupiedSpotsdata.erase(it);
+      availableSpots++;
+    }
+
+    return vehicle;
+  }
+
+  void printDetails() {
+    cout << " -- parking lot details-- " << endl;
+
+    cout << " Capacity : " << capacity << endl;
+    cout << " Occupied Spots : " << (capacity - availableSpots) << endl;
+    cout << " Available Spots Count : " << availableSpots << endl;
+  }
+
+  void printOccupancy() {
+    cout << " -- parking lot occupancy list details-- " << endl;
+    for (auto& soot : spots) {
+      spot->printDetails();
+    }
+  }
+};
